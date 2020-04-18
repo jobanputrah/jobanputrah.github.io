8a40dde5c23082aeac4ca7c5b44b5b265ed987df
Harsh Jobanputra <hjobanpu@stevens.edu>
2020-04-18 05:44:11 -0400

    Add quizapp

---
+++ quizapp/precache-manifest.b0c299fe4bdefef276dcc77a46929615.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3ff5e4de5145872bf7af69c5999010",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f58bcf90311e35f29606",
+    "url": "./static/css/main.d5c59d91.chunk.css"
+  },
+  {
+    "revision": "6a35b3b294b01dc05867",
+    "url": "./static/js/2.242f3729.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.242f3729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58bcf90311e35f29606",
+    "url": "./static/js/main.7c2a4ab8.chunk.js"
+  },
+  {
+    "revision": "abf70bdbd9e1d6ddab9c",
+    "url": "./static/js/runtime-main.68a04185.js"
+  },
+  {
+    "revision": "7627bc820cce40098e897ca98413d895",
+    "url": "./static/media/loading.7627bc82.svg"
+  }
+]);
